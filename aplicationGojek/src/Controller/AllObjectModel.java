748be28ea9bd8c061d183cd53795a07376b730de
Naufal <73730231+ahmadnaufal077@users.noreply.github.com>
2021-12-22 23:07:33 +0700

    Delete aplicationGojek directory

--- aplicationGojek/src/Controller/AllObjectModel.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-import Model.*;
-
-//inisialisasi objc model dgn buat objc baru dengan nama mahasis utk digunakan pd classcontrol
-public class AllObjectModel {
-    public static  UserModel userModel = new UserModel();
-    public static AdminModel adminModel = new AdminModel();
-}
--- aplicationGojek/src/Controller/ControllerInterfaces.java
+++
@@ -1,6 +0,0 @@
-package Controller;
-
-public interface ControllerInterfaces {
-    int login(String email, String password);
-    void insertData(String nama, String email, String password, String no_telp);
-}
