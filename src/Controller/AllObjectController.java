8d5f055c0468ca5f19236e898194f93daf9ce3ca
Naufal <73730231+ahmadnaufal077@users.noreply.github.com>
2022-01-06 09:50:16 +0700

    Delete src directory

--- src/Controller/AllObjectController.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-import Controller.AdminController;
-import Controller.UserController;
-
-public class AllObjectController {
-    public static AdminController admin = new AdminController();
-    public static UserController user = new UserController();
-}
