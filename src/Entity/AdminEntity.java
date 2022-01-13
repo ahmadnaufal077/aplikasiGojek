91a20416298154a0159106db1a31179078824b96
Naufal <73730231+ahmadnaufal077@users.noreply.github.com>
2022-01-13 09:34:49 +0700

    Delete src directory

--- src/Entity/AdminEntity.java
+++
@@ -1,7 +0,0 @@
-package Entity;
-
-public class AdminEntity extends UserEntityAbstract {
-    public AdminEntity(String nama, String email, String password, String noTelp){
-        super(nama,password,noTelp,email);
-    }
-}
